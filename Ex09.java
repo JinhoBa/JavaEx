e5e014eb692b2951e9fa42e7cf654d23c0b8e483
JinhoBa <127280695+JinhoBa@users.noreply.github.com>
2023-03-21 09:56:45 +0900

    Delete Ex09.java

--- Ex09.java
+++
@@ -1,9 +0,0 @@
-
-public class Ex09 {
-	public static void main(String[] args) {
-		Automobile auto = new Automobile();
-		
-		auto.upSpeed(250);
-		System.out.println("승용차의 속도는 " + auto.getSpeed() + "km입니다.");
-	}
-}
